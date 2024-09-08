373717e008bcc0b9c937076e05e72ac06e21a16e
ZenGacilan <potapetekau@gmail.com>
2024-09-08 20:50:46 +0800

    initial commit

---
+++ add.js
@@ -0,0 +1,9 @@
+let firstFavNum = 21; 
+let secondFavNum = 23;
+
+console.log("Addition:", firstFavNum + secondFavNum);
+console.log("Subtraction:", firstFavNum - secondFavNum);
+console.log("Multiplication:", firstFavNum * secondFavNum);
+console.log("Division:", firstFavNum / secondFavNum);
+console.log("Modulus:", firstFavNum % secondFavNum);
+console.log("Exponentiation (Power):", firstFavNum ** secondFavNum);
